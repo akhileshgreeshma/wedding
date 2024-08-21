43a9197af02c5ca3d2f698fec0faed137d47d5be
akhileshgreeshma <akhileshgreeshma2024@gmail.com>
2024-08-21 23:14:49 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+  $(".image__slider").slick({
+   slidesToShow: 3,
+   infinite:false,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000
+     // dots: false, Boolean
+    // arrows: false, Boolean
+  });
